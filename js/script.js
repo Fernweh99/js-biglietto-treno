de6ce780df1113df22628fab46cf04ebf5dd3e1d
Fernweh99 <daniele.dimento@gmail.com>
2022-06-21 15:44:11 +0200

    delete console log

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 // Recupero l'elemento p dal DOM
 ticketPrice = document.getElementById("prezzo-finale");
-console.log(ticketPrice);
 
 // Chiedo all'utente il numero di chilometri che vuole percorrere
 const kilomRun = parseInt(prompt("Quanti kilometri vuoi percorrere?", "20"));
-console.log(kilomRun);
 
 // Chiedo all'utente la sua età
 const userAge = parseInt(prompt("Quanti anni hai?", "17"));
-console.log(userAge);
 
 // Valido i dati inseriti dall'utente
 if (isNaN(kilomRun) | isNaN(userAge)) {
@@ -18,11 +15,9 @@ if (isNaN(kilomRun) | isNaN(userAge)) {
 else {
     // dichiaro constante prezzo per kilometri
     const priceForKilom = 0.21;
-    console.log(priceForKilom);
     
     // dichiaro un altra variabile con l'operazione che calcola il prezzo finale x tot kilometri
     let priceFinal = kilomRun * priceForKilom;
-    console.log(priceFinal);
     
     // Verifico se l'utente rientra tra gli under 18 o over 65 per un eventuale sconto
     let discount = 0
@@ -34,11 +29,8 @@ else {
         discount = priceFinal * 0.4;
     }
     
-    console.log(discount);
-    
     // Calcolo prezzo finale
     priceFinal += - discount;
-    console.log(priceFinal.toFixed(2));
     
     // Output su pagina il prezzo del biglietto
     ticketPrice.innerHTML = `Il prezzo del tuo biglietto è: <strong>${priceFinal.toFixed(2)}€</strong>`;
