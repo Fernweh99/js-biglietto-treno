cbf55dd82bf6a137bfda5ca2537f7047e5f8004b
Fernweh99 <daniele.dimento@gmail.com>
2022-06-21 14:55:32 +0200

    Recover element p from DOM

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Recupero l'elemento p dal DOM
+prezzoFinale = document.getElementById("prezzo-finale");
+console.log(prezzoFinale);
+// Chiedo all'utente il numero di chilometri che vuole percorrere
+// Chiedo all'utente la sua età
+// dichiaro constante prezzo per kilometri
+// dichiaro un altra costante con l'operazione che calcola il prezzo finale x tot kilometri
+// Verifico se l'utente rientra tra gli under 18 o over 65 per un eventuale sconto
+// Calcolo prezzo finale
+// Output su pagina il prezzo del biglietto
